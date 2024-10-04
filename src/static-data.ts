46aa3b3ec31a7d2b70699efc93a20bd9ab7317a5
Kedar Basutkar <41827102+kedaroo@users.noreply.github.com>
2024-10-04 15:37:18 +0530

    add static data with thumbnails

---
+++ src/static-data.ts
@@ -0,0 +1,38 @@
+export const STATIC_DATA = [
+    {
+      id: 1,
+      type: "bank-draft",
+      title: "Bank Draft",
+      position: 0,
+      thumbnail: "/cat-1.jpeg",
+    },
+    {
+      id: 2,
+      type: "bill-of-lading",
+      title: "Bill of Lading",
+      position: 1,
+      thumbnail: "/cat-2.jpeg",
+    },
+    {
+      id: 3,
+      type: "invoice",
+      title: "Invoice",
+      position: 2,
+      thumbnail: "/cat-3.jpeg",
+    },
+    {
+      id: 4,
+      type: "bank-draft-2",
+      title: "Bank Draft 2",
+      position: 3,
+      thumbnail: "/cat-4.jpg",
+    },
+    {
+      id: 5,
+      type: "bill-of-lading-2",
+      title: "Bill of Lading 2",
+      position: 4,
+      thumbnail: "/cat-5.jpg",
+    },
+  ];
+  
